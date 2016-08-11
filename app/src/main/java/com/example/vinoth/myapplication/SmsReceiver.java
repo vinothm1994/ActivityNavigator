21c80d16d8fbab133568af933f7cc29376e9c49e
vinoth <vinoth.m1994@gmail.com>
2016-08-11 09:33:01 +0530

    new

---
+++ app/src/main/java/com/example/vinoth/myapplication/SmsReceiver.java
@@ -0,0 +1,25 @@
+package com.example.vinoth.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by vinoth on 10/8/16.
+ */
+public  class SmsReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Log.i("vinothh","ddddddd on");
+        if (intent.getAction().intern() == Intent.ACTION_AIRPLANE_MODE_CHANGED) {
+
+            Log.i("vinothh","ddddddd on");
+            Toast.makeText(context,"onnnn",Toast.LENGTH_SHORT).show();
+
+        }
+
+    }
+}
